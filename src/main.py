b7fcfbcc60fb7a46d88e62b2070d0df5c013ebdd
danialasghar <danial.aw@gmail.com>
2020-01-25 16:54:49 -0500

    read

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from structs import node
 
+#Main function
+def printNextMove(player, player1Mancala, player1Marbles, player2Mancala, player2Marbles):
+    print ""
+
+#Read Input
+player = input()
+mancala1 = input()
+mancala1_marbles = [int(i) for i in raw_input().strip().split()]
+mancala2 = input()
+mancala2_marbles = [int(i) for i in raw_input().strip().split()]
+print printNextMove(player, mancala1, mancala1_marbles, mancala2, mancala2_marbles)
+
 '''
 HERE IS WHERE THE MAIN GAME LOOP WILL GO
+COPIED FROM HACKERRANK
+
 '''
 
-def score_moteCarlo(node): #Generates the cost of this node based on how well it does in a Monte-carlo sim
+def score_monteCarlo(node): #Generates the cost of this node based on how well it does in a Monte-carlo sim
     #While(within time limit)
     #   sim_node.boardstate = node.boardstate <-- Create simulation node? Like a kernel.
     #   while(board not in a win state and within time limit): 
@@ -21,3 +35,9 @@ def score_moteCarlo(node): #Generates the cost of this node based on how well it
 
 def abPrune(node):
     return 0
+
+def hScore(): 
+    # 1. My vs other mancala
+    # 2. Empty node (don't pick)
+    # 3. Keep as many counters on players side of board
+    # 4. Allow for an extra move
