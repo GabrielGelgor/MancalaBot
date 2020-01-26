81b9c1b2ae9c77f35a4be41b8029ffd22238a8d8
GabrielGelgor <gabriel.gelgor@uoit.net>
2020-01-25 21:43:22 -0500

    Session 1 complete: move logic ready to be transition into code. heuristics partially completed.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 from structs import node
-
-#Main function
-def printNextMove(player, player1Mancala, player1Marbles, player2Mancala, player2Marbles):
-    print ""
+import math
 
 #Read Input
-player = input()
-mancala1 = input()
+player = int(input())
+mancala1 = [int(input())]
 mancala1_marbles = [int(i) for i in raw_input().strip().split()]
-mancala2 = input()
+mancala2 = [int(input())]
 mancala2_marbles = [int(i) for i in raw_input().strip().split()]
-print printNextMove(player, mancala1, mancala1_marbles, mancala2, mancala2_marbles)
+print(printNextMove(player, mancala1, mancala1_marbles, mancala2, mancala2_marbles))
+
+#Main function
+def printNextMove(player, player1Mancala, player1Marbles, player2Mancala, player2Marbles):  
+    state1 = mancala1_marbles[:]
+    state2 = mancala2_marbles[:] 
+    boardState = [state1, state2]
+    
+    
+    print("output")
+
 
 '''
 HERE IS WHERE THE MAIN GAME LOOP WILL GO
@@ -33,11 +40,43 @@ def score_monteCarlo(node): #Generates the cost of this node based on how well i
     #           break #
     return 0
 
-def abPrune(node):
+def abPrune(node):# send in self, board, ply
+    
+
+    
     return 0
 
-def hScore(): 
-    # 1. My vs other mancala
-    # 2. Empty node (don't pick)
-    # 3. Keep as many counters on players side of board
-    # 4. Allow for an extra move
+def hScore(player, move): 
+
+    #Calculate score difference between both players' mancala
+    if (player == 1):
+        H1 = mancala1 - mancala2
+    if (player == 2):
+        H1 = mancala2 - mancala1
+        
+    #Maximize counters on players own side of board
+    if (player == 1): #If player 1 calculate number of counters on your side
+        mySide = boardState[0][0] + boardState[0][1] + boardState[0][2] + boardState[0][3] + boardState[0][4] + boardState[0][5]
+        otherSide = boardState[1][0] + boardState[1][1] + boardState[1][2] + boardState[1][3] + boardState[1][4] + boardState[1][5]
+        H2 = mySide - otherSide
+    if (player == 2): #else if player 2 determine the counters on their side 
+        mySide = boardState[1][0] + boardState[1][1] + boardState[1][2] + boardState[1][3] + boardState[1][4] + boardState[1][5]
+        otherSide = boardState[0][0] + boardState[0][1] + boardState[0][2] + boardState[0][3] + boardState[0][4] + boardState[0][5]
+        H2 = mySide - otherSide
+
+    #Calculate if you get an extra move
+    H3 = 0
+    if (player == 1): #if player 1, move distance to mancala = 0, set h3 to 1
+        numOfMarbles = boardState[0][move]
+        if ()
+    if (player == 2): #if player 2, move distance to mancala = 0, set h3 to 1
+        numOfMarbles = boardState[1][move]
+        
+
+
+    #Try to end turn on an empty pit
+    H4 = 0
+    
+
+#'''https://www.hackerrank.com/challenges/mancala6'''
+#"https://github.com/naigutstein/Mancala/blob/master/Player.py" #ref guide i guess
--- src/structs.py
+++ src/structs.py
@@ -9,22 +9,54 @@ class node:
 
     def expand(self):
         #for every possible child
-            #calculate new board state from the current state. Base move on iterator
+        for child in range(1, len(self.state[0])):
+            break
+            #calculate new board state from the current state. Base move on iterator  
+            #for every marble in this hole:
+                #
             #generate node using current player as parent, and state
             #Add node to frontier
         return 0
 
     def takeMove(self, move, player):
-        #for the number of marbles in the selected hole
-            #drop a marble in the next hole.
-        
-        #Is the first hole index + number of marbles your mancala?
-            #If so, ensure that you get an extra turn
+        cur_state = self.state[:]
 
-        #Return the new state, as well as the next player in the case of consecutive turns
+        #Determine the row and direction for this move based on the player
+
+        #Remove all the marbles from the hole they were taken from
+        #Add these marbles to your "hand" 
+        #Set a counter for how many moves you've taken (start at 1)
+        #while you still have marbles left in your hand:
+        #   determine the position of where you should place the next marble{
+        #   counter(holes traveled so far) * your direction + the original position of your hole.}
+        # 
+        #   Is this marble within your list? {
+        #       yes (is destination between 0 and the last hole?): place the marble, no problem.
+        # 
+        #       no: row switch! do the following
+        #           have you hit your mancala? check the row you just finished against the player. If yes, drop a marble. check if this is your last marble.
+        #           switch your row
+        #           switch your direction
+        #           based on which row you just entered, place where your 'move' now originates from.
+        #           set counter (i) to -1 so your displacement will end up starting at 0 when you go back to the top 
+        #           }
+        # 
+        #   add one to displacement counter#
+
+        #Return the new state, as well as the next player in the case of consecutive turns, return the side of the board you finally landed on.
         return 0
 
     def evalBoard(self): #Determine whether or not this board is in a win state
         #Scan the current board, if it is in a win state, return so. Else, return not.
-        return 0
+        end = false
+        for i in range(len(self.state)) 
+            if (i == 0):
+                for j in range(len(self.state[i]))
+                    if (self.state[i][j] != 0):
+                        end = true
+            if (i == 1):
+                for j in range(len(self.state[i]))
+                    if (self.state[i][j] != 0):
+                        end = true
+        return end
 
